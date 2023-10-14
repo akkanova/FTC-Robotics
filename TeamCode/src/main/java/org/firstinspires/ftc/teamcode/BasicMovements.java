4916ae01677ba4f68f8e91cb7f9e0b460e89e8f7
Akkanova <alfredrickfantone@gmail.com>
2023-10-13 22:21:14 -0600

    Cleanup code and add more documentations.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovements.java
@@ -4,87 +4,97 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name= "Main Movements", group= "Linear OpMode")
+@TeleOp(name= "Basic Movements", group= "Linear OpMode")
 
+/*
+* This will serve as proof of concept for our robot, only containing the basic movements.
+* Only covering Forward, Backwards, Side-ways Strafing, and Rotation.
+*
+* Use Hardware Configuration: `Config-2023`
+*
+* =================================IMPORTANT===================================
+* Please ensure that the GamePad controller is plugged into the furthest port
+* from the power switch. Diagram shown here:
+*
+*                 -------------------
+*                 |  Blank          |
+*                 |       Screen    |
+*                 -------------------
+*  Ethernet Port   ⮥  ⭡            ⮤ Power button Here
+*                     |
+*              ->   PLUG HERE!!  <-
+*
+* =================================IMPORTANT===================================
+* */
 public class BasicMovements extends LinearOpMode {
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    // All Motors
+    private DcMotor frontLeftM;
+    private DcMotor frontRightM;
+    private DcMotor backLeftM;
+    private DcMotor backRightM;
 
-    private final double MIN_MOTOR_POWER = -1.0;
-    private final double MAX_MOTOR_POWER = 1.0;
+    private final double MAX_REVERSE = -1.0;
+    private final double MAX_FORWARD = 1.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        // Hardware Preparations
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        // Rough Chassis Sketch
+        // `frontLeft` 0| |   | |0 `frontRight`
+        //              | |   | |
+        //              | |= =| |
+        // `backLeft`  0| |   | |0 `backRight`
+
+        frontLeftM  = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRightM = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeftM   = hardwareMap.get(DcMotor.class, "backLeft");
+        backRightM  = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeftM.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightM.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeftM.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightM.setDirection(DcMotorSimple.Direction.FORWARD);
 
         telemetry.addData("Status", "Hardware Prepared");
+        telemetry.addLine("Press the PLAY button to start.");
         telemetry.update();
 
+        // Wait for the user to click the PLAY BUTTON
         waitForStart();
+        ElapsedTime totalRuntime = new ElapsedTime();
 
         while(opModeIsActive()) {
             double stickPitch = -gamepad1.left_stick_y;
             double stickRoll = gamepad1.left_stick_x;
 
-            double leftPower  = Range.clip(stickPitch + stickRoll, MIN_MOTOR_POWER, MAX_MOTOR_POWER);
-            double rightPower = Range.clip(stickPitch - stickRoll, MIN_MOTOR_POWER, MAX_MOTOR_POWER);
+            double leftPower  = clampPower(stickPitch + stickRoll);
+            double rightPower = clampPower(stickPitch - stickRoll);
 
-//
-//            if (gamepad1.dpad_up) {
-//                frontLeft.setPower(MAX_MOTOR_POWER);
-//                frontRight.setPower(MAX_MOTOR_POWER);
-//                backLeft.setPower(MAX_MOTOR_POWER);
-//                backRight.setPower(MAX_MOTOR_POWER);
-//
-//            } else if (gamepad1.dpad_down) {
-//                frontLeft.setPower(MIN_MOTOR_POWER);
-//                frontRight.setPower(MIN_MOTOR_POWER);
-//                backLeft.setPower(MIN_MOTOR_POWER);
-//                backRight.setPower(MIN_MOTOR_POWER);
-//
-//            } else if (gamepad1.dpad_left) {
-//                frontLeft.setPower(MAX_MOTOR_POWER);
-//                frontRight.setPower(MIN_MOTOR_POWER);
-//                backLeft.setPower(MAX_MOTOR_POWER);
-//                backRight.setPower(MIN_MOTOR_POWER);
-//
-//            } else if (gamepad1.dpad_right) {
-//                frontLeft.setPower(MIN_MOTOR_POWER);
-//                frontRight.setPower(MAX_MOTOR_POWER);
-//                backLeft.setPower(MIN_MOTOR_POWER);
-//                backRight.setPower(MAX_MOTOR_POWER);
-//
-//            } else {
-//                frontLeft.setPower(0);
-//                frontRight.setPower(0);
-//                backLeft.setPower(0);
-//                backRight.setPower(0);
-//            }
+            frontLeftM.setPower(leftPower);
+            frontRightM.setPower(rightPower);
+            backRightM.setPower(rightPower);
+            backLeftM.setPower(leftPower);
 
-            frontLeft.setPower(leftPower);
-            frontRight.setPower(rightPower);
-            backRight.setPower(rightPower);
-            backLeft.setPower(leftPower);
+            telemetry.addData("Status","Total Runtime %.3f s", totalRuntime.seconds());
 
-            telemetry.addData("FL Motor Power", frontLeft.getPower());
-            telemetry.addData("FR Motor Power", frontRight.getPower());
-            telemetry.addData("BL Motor Power", backLeft.getPower());
-            telemetry.addData("BR Motor Power", backRight.getPower());
+            telemetry.addData("FL Motor Power", frontLeftM.getPower());
+            telemetry.addData("FR Motor Power", frontRightM.getPower());
+            telemetry.addData("BL Motor Power", backLeftM.getPower());
+            telemetry.addData("BR Motor Power", backRightM.getPower());
 
-            telemetry.addData("Status", "Running");
             telemetry.update();
         }
+    }
+
+    /** Given the power, ensure that it doesn't exceed minimum and maximum */
+    public double clampPower(double input) {
+        if (input > MAX_FORWARD)
+            return MAX_FORWARD;
+        else if (input < MAX_REVERSE)
+            return MAX_REVERSE;
+        else return input;
     }
 }
