587d0d229228ff90097416c1a7c6cfd706471f6a
Akkanova <alfredrickfantone@gmail.com>
2023-11-15 19:08:13 -0700

    Add aprilTagProcessor to ComputerVision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/all_purpose/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/all_purpose/ComputerVision.java
@@ -1,26 +1,57 @@
 package org.firstinspires.ftc.teamcode.all_purpose;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+/**
+ * Contains all the processors, and setup code for Computer Vision.
+ * Computer Vision Pipeline Overly-Simplified:
+ *
+ *   WEBCAM-SNAPSHOT-IMAGE  ->  APRIL-TAG-PROCESSOR  ->  FINAL-IMAGE-WITH-BOXES & MAGICAL-POS-DATA
+ */
 public class ComputerVision {
-    public final VisionPortal visionPortal;
+    public AprilTagProcessor aprilTagProcessor;
+    public VisionPortal visionPortal;
+
+    private final WebcamName webcamName;
 
     public ComputerVision(HardwareMap hardwareMap) {
-        this(hardwareMap, false);
+        this.webcamName = hardwareMap.get(WebcamName.class, "webcam");
     }
 
-    public ComputerVision(HardwareMap hardwareMap, boolean enablePreview) {
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+    // Previews are CPU intensive, so they are disabled by default..
+    public void initialize() { initialize(false); }
+    public void initialize(boolean enablePreview) {
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setDrawCubeProjection(enablePreview)
+                .setDrawTagOutline(enablePreview)
+                .setDrawTagID(enablePreview)
+                .setDrawAxes(enablePreview)
+                .build();
+
         visionPortal = new VisionPortal.Builder()
+                .setCameraResolution(new Size(640, 480)) // Pre-Calibrated Resolution
+                .setAutoStopLiveView(!enablePreview)
+                .addProcessor(aprilTagProcessor)
                 .enableLiveView(enablePreview)
                 .setCamera(webcamName)
                 .build();
     }
 
-    public void start() {
+    public void pause() {
+        visionPortal.stopStreaming();
+    }
+
+    public void resume() {
         visionPortal.resumeStreaming();
+    }
+
+    public void destroy() {
+        visionPortal.close();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/OpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/OpenCV.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.all_purpose.ComputerVision;
 import org.firstinspires.ftc.teamcode.all_purpose.HardwareManager;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.teamcode.base.SelfDriving;
 
 @Autonomous(name = "OpenCV Test", group = "Test")
@@ -13,7 +14,7 @@ public class OpenCV extends SelfDriving {
     @Override
     public void runOpMode() {
         hardwareManager = new HardwareManager(hardwareMap);
-        computerVision = new ComputerVision(hardwareMap, true);
+        computerVision = new ComputerVision(hardwareMap);
 
         waitForStart();
         runAutonomous();
@@ -21,6 +22,22 @@ public class OpenCV extends SelfDriving {
 
     @Override
     protected void runAutonomous() {
-        computerVision.start();
+        computerVision.initialize();
+
+        telemetry.addLine("Open the 3 Dots on the Top-Right.");
+        telemetry.addLine("Then Select 'Camera Stream'");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+            // Intentional Idle Loop
+            for (AprilTagDetection detection : computerVision.aprilTagProcessor.getDetections()) {
+                // Update Telemetry ...
+            }
+
+            telemetry.update();
+            idle();
+        }
+
+        computerVision.destroy();
     }
 }
